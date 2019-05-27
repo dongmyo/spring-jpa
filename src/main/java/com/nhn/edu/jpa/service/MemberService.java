22ae2bca0fa2ceef47df3778dab11a7b207edad5
신동민/NE <dongmyo@nhnent.com>
2019-05-27 18:07:08 +0900

    * 복합키

--- src/main/java/com/nhn/edu/jpa/service/MemberService.java
+++ src/main/java/com/nhn/edu/jpa/service/MemberService.java
@@ -20,22 +20,30 @@ public class MemberService {
 
     @Transactional
     public void doSomething() {
-        MemberDetail memberDetail1 = new MemberDetail();
-        memberDetail1.setType("type1");
-        memberDetail1.setDescription("member1-type1");
-
-        MemberDetail memberDetail2 = new MemberDetail();
-        memberDetail2.setType("type2");
-        memberDetail2.setDescription("member1-type2");
-
         Member member = new Member();
         member.setName("member1");
         member.setCreateDate(LocalDateTime.now());
 
+        Member savedMember = memberRepository.save(member);
+
+        MemberDetail.Pk pk1 = new MemberDetail.Pk();
+        pk1.setMemberId(savedMember.getMemberId());
+        pk1.setType("type1");
+
+        MemberDetail memberDetail1 = new MemberDetail();
+        memberDetail1.setPk(pk1);
+        memberDetail1.setDescription("member1-type1");
+
+        MemberDetail.Pk pk2 = new MemberDetail.Pk();
+        pk2.setMemberId(savedMember.getMemberId());
+        pk2.setType("type2");
+
+        MemberDetail memberDetail2 = new MemberDetail();
+        memberDetail2.setPk(pk2);
+        memberDetail2.setDescription("member1-type2");
+
         member.getDetails().add(memberDetail1);
         member.getDetails().add(memberDetail2);
-
-        memberRepository.save(member);
     }
 
 }
