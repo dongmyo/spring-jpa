1073339590781a6a9fa08fb3d3716bb503deeeb0
신동민/NE <dongmyo@nhnent.com>
2019-05-27 17:06:24 +0900

    * 복합키

--- src/main/java/com/nhn/edu/jpa/entity/MemberDetail.java
+++ src/main/java/com/nhn/edu/jpa/entity/MemberDetail.java
@@ -1,27 +1,39 @@
 package com.nhn.edu.jpa.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "MemberDetails")
 public class MemberDetail {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "member_detail_id")
-    private Long memberDetailId;
-
-    private String type;
+    @EmbeddedId
+    private Pk pk;
 
     private String description;
+
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+        @Column(name = "member_id")
+        private Long memberId;
+
+        private String type;
+
+    }
 
 }
