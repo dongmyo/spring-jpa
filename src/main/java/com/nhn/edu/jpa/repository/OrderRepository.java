78b62e3f51ab2110d2901ff6b746006bb8218310
신동민/NE <dongmyo@nhnent.com>
2019-05-28 19:25:14 +0900

    * JPQL join fetch를 이용해서 N + 1 문제 해결

--- src/main/java/com/nhn/edu/jpa/repository/OrderRepository.java
+++ src/main/java/com/nhn/edu/jpa/repository/OrderRepository.java
@@ -2,6 +2,14 @@ package com.nhn.edu.jpa.repository;
 
 import com.nhn.edu.jpa.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("select o from Order o inner join fetch o.customer as c "
+           + " left join fetch o.orderItems as oi "
+           + " inner join fetch oi.item as i")
+    List<Order> getOrdersWithAssociations();
+
 }
