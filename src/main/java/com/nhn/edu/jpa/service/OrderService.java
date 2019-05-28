ebffd9dcb4f622ced92d9e98341a59f99b74cb04
신동민/NE <dongmyo@nhnent.com>
2019-05-29 04:00:52 +0900

    * Order에 OrderAttribute에 대한 1:n 연관관계 추가하고 fetch join 시 MultipleBagFetchException 오류 발생

--- src/main/java/com/nhn/edu/jpa/service/OrderService.java
+++ src/main/java/com/nhn/edu/jpa/service/OrderService.java
@@ -3,6 +3,7 @@ package com.nhn.edu.jpa.service;
 import com.nhn.edu.jpa.entity.Customer;
 import com.nhn.edu.jpa.entity.Item;
 import com.nhn.edu.jpa.entity.Order;
+import com.nhn.edu.jpa.entity.OrderAttribute;
 import com.nhn.edu.jpa.entity.OrderItem;
 import com.nhn.edu.jpa.repository.CustomerRepository;
 import com.nhn.edu.jpa.repository.ItemRepository;
@@ -67,10 +68,14 @@ public class OrderService {
         orderItem1_2.setItem(banana);
         orderItem1_2.setQuantity(2L);
 
+        OrderAttribute attribute1_1 = new OrderAttribute();
+        attribute1_1.setAttribute("attr1");
+
         Order order1_1 = new Order();
         order1_1.setCustomer(customer1);
         order1_1.setOrderDate(LocalDateTime.now());
         order1_1.setOrderItems(Arrays.asList(orderItem1_1, orderItem1_2));
+        order1_1.setAttributes(Collections.singletonList(attribute1_1));
         orderRepository.save(order1_1);
 
         Customer customer2 = new Customer();
@@ -91,10 +96,17 @@ public class OrderService {
         orderItem2_2.setItem(banana);
         orderItem2_2.setQuantity(5L);
 
+        OrderAttribute attribute2_1 = new OrderAttribute();
+        attribute2_1.setAttribute("attr1");
+
+        OrderAttribute attribute2_2 = new OrderAttribute();
+        attribute2_2.setAttribute("attr2");
+
         Order order2_2 = new Order();
         order2_2.setCustomer(customer2);
         order2_2.setOrderDate(LocalDateTime.now());
         order2_2.setOrderItems(Collections.singletonList(orderItem2_2));
+        order2_2.setAttributes(Arrays.asList(attribute2_1, attribute2_2));
         orderRepository.save(order2_2);
     }
 
